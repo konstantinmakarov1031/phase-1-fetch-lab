003fb9a67752c36af929bab964968315be453aaf
konstantin <makarovestates@gmail.com>
2022-08-24 16:29:45 -0500

    commit tree 17bb59cf5e2994b874f3168e6f307c605e44dfab

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+    fetch('https://anapioficeandfire.com/api/books')
+    .then(function(response){
+      return response.json();
+    })
+    
   
-}
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -9,6 +13,8 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+
+
   });
 }
 
